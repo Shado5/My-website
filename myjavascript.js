f7cfbe3d42a3adb7dbe37df2314e686e9a102469
Teresa Fares <teresa_fares@DESKTOP-7R5I059.>
2025-03-15 14:00:24 +1100

    links added

--- myjavascript.js
+++ myjavascript.js
@@ -6,3 +6,17 @@ document.addEventListener("DOMContentLoaded", function () {
         bar.style.width = percent + "%";
     }
 });
+// Wait for the DOM content to load before executing the script
+document.addEventListener('DOMContentLoaded', function() {
+    // Select all images with the class 'clickable-image'
+    const clickableImages = document.querySelectorAll('.clickable-image');
+    
+    // Add a click event listener to each image
+    clickableImages.forEach(image => {
+        image.addEventListener('click', () => {
+            // Get the link from the data-link attribute
+            const link = image.getAttribute('data-link');
+             window.open(link, '_blank'); // Redirect to the link
+        });
+    });
+});
